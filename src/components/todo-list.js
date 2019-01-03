882015337d98ec5d82fdcfddc95a610b885801f7
Surik266 <surik.pozoyan@gmail.com>
2019-01-03 16:09:41 +0400

    Added application components

---
+++ src/components/todo-list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoListItem from './todo-list-item'
+
+const TodoList = () => {
+  return(
+    <ul>
+      <li><TodoListItem/></li>
+      <li><TodoListItem/></li>
+    </ul>
+  );
+};
+
+export default TodoList
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-  <div>
-    <h1>My Todo List</h1>
-    <input placeholder="search"/>
-    <ul>
-      <li>Learn React</li>
-      <li>Build Awesome App</li>
-    </ul>
-  </div>
+import AppHeader from './components/app-header'
+import SearchPanel from './components/search-panel'
+import TodoList from './components/todo-list'
 
-);
+const App = () =>{
 
-ReactDOM.render(el,document.getElementById('root'));
+  return(
+    <div>
+      <AppHeader/>
+      <SearchPanel/>
+      <TodoList/>
+    </div>
+  );
+}
+
+
+ReactDOM.render(<App/>,document.getElementById('root'));
